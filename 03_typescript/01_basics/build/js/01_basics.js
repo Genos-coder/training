7104fa76e7fa07719c45b5fa9ee1aed1aac862fe
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-12-06 23:58:54 +0530

    feat starting with typescript

---
+++ 03_typescript/01_basics/build/js/01_basics.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("this is ts tutorial");
+var num = 12;
+// const firstName: string = "gex";
---
+++ 03_typescript/01_basics/src/01_basics.ts
@@ -0,0 +1,5 @@
+console.log("this is ts tutorial");
+
+const num: number = 12;
+
+// const firstName: string = "gex";
