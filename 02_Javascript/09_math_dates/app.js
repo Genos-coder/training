fc88ac2a7c8b1bdf63e794313c48465db7572d7a
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-12-06 23:59:35 +0530

    feat dates and math in js

--- 02_Javascript/09_math_dates/app.js
+++ 02_Javascript/09_math_dates/app.js
@@ -1,15 +1,21 @@
 console.log(Math.PI);
 console.log(Math.E);
 
-console.log(Math.round(1.2));
+console.log(Math.round(1.5));
 console.log(Math.floor(1.8)); //round off the number to the lowest near integer
 console.log(Math.ceil(1.2)); // round off the number to the largest near integer
 console.log(Math.sqrt(4)); //output:2
 console.log(Math.min(23, 111, 445, 1234));
 console.log(Math.max(23, 111, 445, 1234));
-console.log(Math.abs(-10)); //output:10 (taking modulus of number)
+console.log(Math.abs(-10)); //out+put:10 (taking modulus of number)
 console.log(Math.pow(2, 2)); //Math.pow(number,power)
 
+const randomNum = Math.random(); // output:random number between 0 to 1
+console.log(randomNum);
+//Question: create the rolling dice using Math.random()
+
+const faceNum = Math.ceil(Math.random() * 6); //output: between 1 to 6
+console.log(faceNum);
 //DATES In JS
 
 const date1 = new Date();
@@ -23,6 +29,7 @@ console.log(date1.getMinutes());
 console.log(date1.getSeconds());
 console.log(date1.getMilliseconds());
 console.log(date1.setTime(5 * 60 * 1000)); //setting 5 hours using milliseconds
+
 console.log(date1.setFullYear(2024));
 console.log(date1.setMonth(10)); // here 10 represents november because of zero based indexing
 console.log(date1.setDate(22));
