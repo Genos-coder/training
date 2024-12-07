a5e38401bd8a483535cd3df06cb16dc613e21ee4
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-12-07 17:20:31 +0530

    feat switch and loops in js

--- 02_Javascript/08_functions/app.js
+++ 02_Javascript/08_functions/app.js
@@ -65,3 +65,75 @@ const addNumbers2 = (a, b) => {
 const addNumbers3 = (a, b) => a + b; // if you want to return single thing you can remove the parenthesis (implicit return)
 
 const multiplyNum = (a, b) => a + b;
+
+//Rest parameter
+
+//Using rest parameter we can take n numbers of arguments in function
+function combineNumbers(a, b, ...numArr) {
+  // always write rest parameter at the end after all normal parameters
+  console.log(numArr.toString());
+}
+
+combineNumbers(1, 2, 3, 4);
+
+//Spread syntax in js (we can use spread to destructuring)
+
+//with objects
+const batmanMovie = {
+  title: "The Batman",
+  releaseYear: 2022,
+  director: "Matt Reeves",
+};
+
+//Providing same key name to variable as of batmanMovie
+const { title } = { ...batmanMovie };
+
+console.log(title);
+
+//with arrays
+
+const arr = ["bruce", "wayne"];
+
+// In array we can provide different variable name which can assign the values of spread array to that variable
+const [firstName, lastName] = [...arr];
+
+console.log(firstName, lastName);
+
+//assigning different variable name to object keys using spread
+
+const sport = {
+  name: "cricket",
+  players: 11,
+  outdoor: true,
+};
+
+const { players: playingXi, name } = { ...sport };
+
+console.log(playingXi);
+
+const day = prompt("Enter the day").toLowerCase();
+
+switch (day) {
+  case "monday": {
+    console.log("busy day");
+    alert("busy day");
+    break;
+  }
+
+  default: {
+    console.log("not so busy");
+  }
+}
+
+//break and continue with loops
+for (var i = 0; i < 20; i++) {
+  if (i <= 5) {
+    //it will skip the number till 5
+    continue;
+  } else if (i >= 15) {
+    console.log(i); // it will print number from 6 to 15 then it will break the loop
+    break;
+  } else {
+    console.log(i);
+  }
+}
